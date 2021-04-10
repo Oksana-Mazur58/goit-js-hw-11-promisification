8bac7492c5e5d01808d8bc215965522ac2851d0d
Oksana-Mazur58 <72046543+Oksana-Mazur58@users.noreply.github.com>
2021-04-10 22:33:08 +0300

    Добавила 3 завдання

---
+++ js/task2.js
@@ -0,0 +1,20 @@
+
+//Task 2
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+
+    const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+  return Promise.resolve(updatedUsers)
+
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+console.log(toggleUserState(users, 'Ajax').then(logger));
